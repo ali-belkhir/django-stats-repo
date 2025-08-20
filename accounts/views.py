5a90bc27485938e6bf012150fe4231c6f962b84f
Ali <belkhir_ali@outlook.fr>
2025-08-20 13:42:23 +0100

    Initial commit - Django stats app

---
+++ accounts/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.contrib  import messages
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect("/home/")
+        else:
+            messages.info(request, 'Invalid credentials.')
+            return redirect('login')
+    else:
+        return render(request, "login.html")
+
+
+def lo_gout (request):
+    logout(request)
+    return redirect('login')
+
+
+def register(request):
+    return render(request, "register.html")
